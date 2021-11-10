551de45a69508ec5bada1e6030a836c9dbc26345
Erik Gonzalez <erikhuboy@gmail.com>
2021-11-09 20:21:34 -0500

    update changes

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -8,12 +8,12 @@ class Grupo:
 
     grado = None
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo ordinado", asignaturas=[], estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
